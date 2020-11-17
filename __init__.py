5a710636813b780f2081f64faf3a2f4f173d29c4
Dani Hodovic <dani.hodovic@gmail.com>
2020-11-17 18:42:37 +0100

    Add Samson generic webhook command

--- __init__.py
+++ __init__.py
@@ -9,6 +9,7 @@ from src.cloudflare import cloudflare
 from src.docker_commands import docker
 from src.grafana import grafana
 from src.postgres import postgres
+from src.samson import samson
 
 # https://github.com/pallets/click/issues/448#issuecomment-246029304
 core._verify_python3_env = lambda: None
@@ -24,4 +25,5 @@ if __name__ == "__main__":
     cli.add_command(postgres)
     cli.add_command(docker)
     cli.add_command(cloudflare)
+    cli.add_command(samson)
     cli()
---
+++ src/samson.py
@@ -0,0 +1,41 @@
+import os
+
+import click
+import requests
+from git import Repo
+
+
+@click.group()
+def samson():
+    pass
+
+
+@samson.command()
+@click.option(
+    "--url",
+    type=str,
+    help="Webhook url.",
+    required=True,
+)
+def generic_webhook(url):
+    repo = Repo(os.getcwd())
+    res = requests.post(
+        url,
+        json={
+            "deploy": {
+                "branch": repo.active_branch.name,
+                "commit": {
+                    "sha": repo.head.commit.hexsha,
+                    "message": repo.head.commit.message,
+                },
+            }
+        },
+    )
+    res.raise_for_status()
+    data = res.json()
+    if not data["deploy_ids"]:
+        click.echo(click.style(data["messages"], fg="red", bold=True))
+        raise click.Abort()
+    click.echo(
+        click.style(f"Deployment succeeded {data['deploy_ids']}", fg="green", bold=True)
+    )
