6cb6646d970cc73931dd8813838843d1d30ff3bb
Dani Hodovic <dani.hodovic@gmail.com>
2021-02-15 17:06:14 +0200

    Add command to open taskwarrior task in editor

--- src/taskwarrior.py
+++ src/taskwarrior.py
@@ -1,6 +1,13 @@
 # pylint: disable=invalid-name
+import json
+import subprocess
+from datetime import timedelta
+
 import click
-from tasklib import TaskWarrior
+import dateutil.parser
+import yaml
+from tasklib import Task, TaskWarrior
+from tasklib.serializing import SerializingObject
 
 
 @click.group()
@@ -50,3 +57,47 @@ def done(task_id):
         for active_task in active_tasks:
             active_task.done()
             click.secho(f"Completed task {active_task['id']}", fg="green")
+
+
+@task.command()
+@click.argument("task_id", required=True)
+@click.option(
+    "--quiet/--no-quiet",
+    default=False,
+    show_default=True,
+)
+def edit(task_id, quiet):
+    """
+    Opens an editor to modify the file in yaml
+    """
+    tw = TaskWarrior(data_location="~/.task", create=False)
+    try:
+        t = tw.tasks.get(id=task_id)
+    except Task.DoesNotExist as ex:
+        click.secho(f"Task id='{task_id}' does not exist", fg="red")
+        raise click.Abort() from ex
+
+    as_dict = json.loads(t.export_data())
+    immutable_keys = t.read_only_fields + ["status"]
+    for key in immutable_keys:
+        if key in immutable_keys:
+            del as_dict[key]
+
+    if "due" in as_dict:
+        due = dateutil.parser.parse(as_dict["due"]) + timedelta(days=1)
+        as_dict["due"] = due.strftime("%Y-%m-%d")
+
+    result = click.edit("---\n" + yaml.dump(as_dict), extension=".yaml")
+    if not result:
+        raise click.Abort()
+
+    modified = yaml.load(result, yaml.FullLoader)
+    if "due" in modified:
+        serializer = SerializingObject(t.backend)
+        due = dateutil.parser.parse(modified["due"])
+        modified["due"] = serializer.timestamp_serializer(due)
+    t._update_data(modified)  # pylint: disable=protected-access
+    t.save()
+
+    if not quiet:
+        subprocess.Popen(["task", "information", task_id])
