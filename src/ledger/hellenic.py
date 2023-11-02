ae88b7540038ebbb319d2af6b267fc723d7501a2
Dani Hodovic <dani.hodovic@gmail.com>
2023-11-01 21:51:34 -0300

    fix: parsing for Hellenic and Revolut

--- src/ledger/hellenic.py
+++ src/ledger/hellenic.py
@@ -1,4 +1,7 @@
 import csv
+import locale
+import re
+import subprocess
 
 import arrow
 
@@ -6,23 +9,26 @@ from .cmd import in_file, ledger, out_file, write_csv
 
 
 def transform_row(row):
-    date = arrow.get(row[3], "DD/MM/YYYY")
+    date = arrow.get(row[2], "DD/MM/YYYY")
     date = date.format("YYYY-MM-DD")
     value = parse_value(row)
-    description = row[4]
+    description = row[3]
     return [date, description, value]
 
 
 def parse_value(row):
+    # Parses "3.426,77" => 3426.77
+    # locale.setlocale(locale.LC_ALL, "sv_SE.utf8")
+    locale.setlocale(locale.LC_ALL, "de_DE.UTF-8")
     try:
-        paid_in = float(row[6].replace(",", ""))
+        paid_in = locale.atof(row[-4])
         if paid_in != 0.0:
             return paid_in
     except ValueError:
         pass
 
-    paid_out = float(row[5].replace(",", ""))
-    return -paid_out
+    paid_out = locale.atof(row[-5])
+    return paid_out
 
 
 @ledger.command()
@@ -30,8 +36,9 @@ def parse_value(row):
 @out_file("/tmp/hellenic-to-reckon.csv")
 def hellenic_to_reckon(file, outfile):
     """
-    Converts Transferwise csv output so that it's parsable by cantino/reckon.
+    Converts Hellenic xls to csv and then parses said csv so that it's parsable by cantino/reckon.
     """
+    # reader = csv.reader(preprocess_revolut_csv(file), delimiter=",")
     reader = csv.reader(file, delimiter=",")
     # Skip header
     next(reader)
